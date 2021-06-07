c3ccc38a95930bbc754d6717540c5ebbd4d88d28
dangbroitsdon <hoodzabyss@gmail.com>
2021-06-07 17:57:26 -0400

    fixed my id being owner, instead of a placeholder
    title

--- bot.py
+++ bot.py
@@ -7,33 +7,33 @@ cluster = MongoClient(mongo_uri)
 db = cluster["(db name)"] # db name goes here, remove parenthesis
 collection = db["(collection name)"] # collection name goes here, remove parenthesis
 client = commands.Bot(command_prefix= '.')
-
+# remove the id and replace it with yours, it was just a simple owner checking system i used.
 @client.command()
 async def load(ctx, extension):
-    id = ctx.message.author.id
-    if id == 301102975500419073:
+    id = str(ctx.message.author.id)
+    if id == "your id":
         client.load_extension(f'cogs.{extension}')
         await ctx.send(f'{extension} is loaded!')
-    elif id != 301102975500419073:
+    elif id != "your id":
         await ctx.send(f'Sorry but you cannot use this command.')
 
 @client.command()
 async def unload(ctx, extension):
-    id = ctx.message.author.id
-    if id == 301102975500419073:
+    id = str(ctx.message.author.id)
+    if id == "your id":
         client.unload_extension(f'cogs.{extension}')
         await ctx.send(f'{extension} is unloaded!')
-    elif id != 301102975500419073:
+    elif id != "your id":
         await ctx.send(f'Sorry but you cannot use this command.')
 
 @client.command()
 async def reload(ctx, extension):
-    id = ctx.message.author.id
-    if id == 301102975500419073:
+    id = str(ctx.message.author.id)
+    if id == "your id":
         client.unload_extension(f'cogs.{extension}')
         client.load_extension(f'cogs.{extension}')
         await ctx.send(f'{extension} is reloaded!')
-    elif id != 301102975500419073:
+    elif id != "your id":
         await ctx.send(f'Sorry but you cannot use this command.')
 
 for file in os.listdir("./cogs"):
